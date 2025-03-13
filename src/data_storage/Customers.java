9a42fc21f82e84fbb9a5611baaad10d839661803
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-03-13 18:38:35 +0300

    Stream API with lambdas used

--- src/data_storage/Customers.java
+++ src/data_storage/Customers.java
@@ -3,13 +3,16 @@ package data_storage;
 import services.customers.Customer;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Customers extends ArrayList<Customer> {
     public Customer getOrCreate(String name) {
-        for (Customer curr : this) {
-            if (curr.getName().equals(name)) {
-                return curr;
-            }
+        Optional<Customer> optionalCustomer = this.stream()
+                .filter(customer -> customer.getName().equals(name))
+                .findAny();
+
+        if (optionalCustomer.isPresent()) {
+            return optionalCustomer.get();
         }
 
         Customer newCustomer = new Customer(name);
